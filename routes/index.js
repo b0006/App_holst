22ba5a66554e4fa2bd30adca3dfc5e9169582ec1
b0006 <qsefthuken@gmail.com>
2018-03-06 16:54:26 +0700

    "Извлек и декодировал все path из SVG"

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,20 @@ router.post('/', function(req, res) {
     // декодируем JSON-ответ
     var items = JSON.parse(item);
 
+    /**
+     * @path {Array} - path из аттрибута d
+     * @data {Array} - декодированные значения координат, полученные из path
+     */
     var path = [];
     var data = [];
+    /**
+     * @arAbsolutePoints {Array} - абсолютные значения координат X и Y
+     * @absoluteX {int} - абсолютное значение X в итерации
+     * @absoluteY {int} - абсолютное значение Y в итерации
+     */
+    var arAbsolutePoints = [];
+    var absoluteX = 0;
+    var absoluteY = 0;
 
     for(var i = 0; i < items.length; i++)
     {
@@ -27,7 +39,35 @@ router.post('/', function(req, res) {
         path.push(items[i]["path"]);
         // декодируем каждый "путь"
         data.push(paths.decode(path[i]));
+
+        // извлекаем абсолютные значения
+        for(var t = 0; t < data[i].length; t++)
+        {
+            if(data[i][t]["rel"] == false)
+            {
+                if (typeof data[i][t]['x'] !== "undefined")
+                {
+                    absoluteX = data[i][t]["x"];
+                    absoluteY = data[i][t]["y"];
+                    arAbsolutePoints.push({ x: absoluteX, y: absoluteY });
+                }
+            }
+            else
+            {
+                if(typeof data[i][t]['x'] !== "undefined")
+                {
+                    try {
+                        absoluteX = absoluteX + data[i][t]["x"];
+                        absoluteY = absoluteY + data[i][t]["y"];
+                        arAbsolutePoints.push({ x: absoluteX, y: absoluteY });
+                    }
+                    catch(e) {}
+                }
+            }
+        }
     }
+
+    console.log(arAbsolutePoints);
 
     // res.send(item);
 });
