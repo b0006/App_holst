9b56e3c644adf7b6cbce4bc25d3ca813398adf5e
b0006 <qsefthuken@gmail.com>
2018-03-17 01:46:50 +0700

    "Извлек и декодировал все path из SVG"

--- public/javascripts/inputSvgFile.js
+++ public/javascripts/inputSvgFile.js
@@ -8,7 +8,7 @@ function handleFileSelect(evt) {
         var importedNode = document.importNode(xml.documentElement, true);
         d3.select("div#vis")
             .append("svg")
-            .attr("viewBox", "0 0 300 150") // изменяем размер SVG
+            .attr("viewBox", "0 0 1200 1200") // изменяем размер SVG
             .each(function() {
                 this.appendChild(importedNode);
             })
@@ -31,10 +31,38 @@ function getSvgFile (svg_file) {
      */
     var paths = object.children;
     var path = [];
+    var index = 0;
 
     for(var i = 0; i < paths.length; i++)
     {
-        path.push({ "path" : paths[i].getAttribute("d")});
+
+        // "пути-одиночки" (которые не в группе) опускаем
+        if(paths[i].children.length == 0) {
+            path.push({"path": paths[i].getAttribute("d")});
+            path.push({"color": paths[i].getAttribute("fill")});
+            path.push({"idColor": index });
+            index++;
+        }
+        // здесь ситуация наоборот
+        else {
+
+            var tmp = paths[i].children;
+
+            for (var t = 0; t < tmp.length; t++) {
+
+                if(tmp[t].getAttribute("d") != null){
+                    path.push({"path": tmp[t].getAttribute("d")});
+                    path.push({"color": tmp[t].getAttribute("fill")});
+                    path.push({"idColor": index});
+                }
+                else {
+                    path.push({"polygon": tmp[t].getAttribute("points")});
+                    path.push({"color": tmp[t].getAttribute("fill")});
+                    path.push({"idColor": index});
+                }
+            }
+            index++;
+        }
     }
 
     // console.log(path);
@@ -44,7 +72,36 @@ function getSvgFile (svg_file) {
         dataType: 'html',
         data: "data=" + JSON.stringify(path),
         success: function (resp) {
-            // alert(resp);
+            var target = JSON.parse(resp);
+            console.log(JSON.parse(resp));
+
+            var text = "";
+            var it = 0;
+
+            // for(var i = 0; i < target.length; i++) {
+            //     if(target[i] == null)
+            //         continue;
+            //     for(var j = 0; j < target[i].length; j++) {
+            //         try {
+            //             text += "var text" + it + " = document.createElementNS('http://www.w3.org/2000/svg', 'text');\n";
+            //             text += "text" + it + ".setAttribute('x', " + target[i][j][0]["x"].x + ");\n";
+            //             text += "text" + it + ".setAttribute('y', " + target[i][j][0]["y"].y + ");\n";
+            //             text += "text" + it + ".setAttribute('style', 'font-size: 20px;');\n";
+            //             text += "text" + it + ".innerHTML = '" + i + "';\n";
+            //             text += "var gg" + it + " = svgDocument.getElementById('test');\n";
+            //             text += "gg" + it + ".appendChild(text" + it + ");\n";
+            //
+            //             it++;
+            //         }
+            //         catch(e) {}
+            //     }
+            // }
+            //
+            //
+            // document.write(
+            //     '<a href="data:text/plain;charset=utf-8,%EF%BB%BF' + encodeURIComponent(text) + '" download="text.txt">text.txt</a>'
+            // )
+
         },
         error: function (xhr, str) {
             alert('Возникла ошибка: ' + xhr.responseCode);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 var paths = require( 'vsvg-paths' );
+var earcut = require("earcut");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +16,7 @@ router.get('/', function(req, res, next) {
  * POST - запрос на все path
  * @type {*|Function}
  */
+
 router.post('/', function(req, res) {
     // получаем ответ от клиента
     var item = req.body.data;
@@ -22,7 +27,7 @@ router.post('/', function(req, res) {
      * @path {Array} - path из аттрибута d
      * @data {Array} - декодированные значения координат, полученные из path
      */
-    var path = [];
+    // var path = [];
     var data = [];
     /**
      * @arAbsolutePoints {Array} - абсолютные значения координат X и Y
@@ -30,46 +35,191 @@ router.post('/', function(req, res) {
      * @absoluteY {int} - абсолютное значение Y в итерации
      */
     var arAbsolutePoints = [];
+
+    for (var i = 0; i < items.length / 3; i++) {
+        arAbsolutePoints.push([]);
+    }
+
     var absoluteX = 0;
     var absoluteY = 0;
 
-    for(var i = 0; i < items.length; i++)
-    {
-        // извлекаем каждый "путь" из аттрибутов d
-        path.push(items[i]["path"]);
-        // декодируем каждый "путь"
-        data.push(paths.decode(path[i]));
+    // var trianglesData;
+    var trianglesResult = [];
+    var triangles = [];
 
-        // извлекаем абсолютные значения
-        for(var t = 0; t < data[i].length; t++)
+    var arPointsCenters = new Array(items.length / 3);
+    var resultCenters = new Array(items.length / 3);
+
+    for (var i = 0, indexData = 0; i < items.length; i = i + 3) {
+
+        arPointsCenters[indexData] = [];
+        trianglesResult[indexData] = [];
+
+        // извлекаем каждый "путь" из аттрибутов d
+        var path = items[i]["path"];
+
+        // если null, то это polygon, а не path
+        if(path != null)
         {
-            if(data[i][t]["rel"] == false)
-            {
-                if (typeof data[i][t]['x'] !== "undefined")
-                {
-                    absoluteX = data[i][t]["x"];
-                    absoluteY = data[i][t]["y"];
-                    arAbsolutePoints.push({ x: absoluteX, y: absoluteY });
-                }
-            }
-            else
-            {
-                if(typeof data[i][t]['x'] !== "undefined")
-                {
-                    try {
-                        absoluteX = absoluteX + data[i][t]["x"];
-                        absoluteY = absoluteY + data[i][t]["y"];
-                        arAbsolutePoints.push({ x: absoluteX, y: absoluteY });
+            // декодируем каждый "путь"
+            data.push(paths.decode(path));
+
+            // извлекаем абсолютные значения
+            for (var t = 0; t < data[indexData].length; t++) {
+                if (data[indexData][t]["rel"] == false) {
+                    if (typeof data[indexData][t]['x'] !== "undefined") {
+                        absoluteX = data[indexData][t]["x"];
+                        absoluteY = data[indexData][t]["y"];
+                        arAbsolutePoints[indexData].push([absoluteX, absoluteY]);
                     }
-                    catch(e) {}
+                }
+                else {
+                    if (typeof data[indexData][t]['x'] !== "undefined") {
+                        try {
+                            absoluteX = absoluteX + data[indexData][t]["x"];
+                            absoluteY = absoluteY + data[indexData][t]["y"];
+                            arAbsolutePoints[indexData].push([absoluteX, absoluteY]);
+                        }
+                        catch (e) {
+                        }
+                    }
                 }
             }
+
+            /**
+             * ТРИАНГУЛЯЦИЯ
+             */
+
+            // ВЫЗЫВАЕМ ФУНКЦИЮ ТРИАНГУЛЯЦИИ
+            var trianglesData = earcut.flatten([arAbsolutePoints[indexData]]);
+            // ЧЕРЕЗ ЭТУ ФУНКЦИЮ ПОЛУЧАЕМ КООРДИАНТЫ ТОЧЕК ТРЕУГОЛЬНИКОВ в arPointsTriangles
+            trianglesResult[indexData] = earcut(trianglesData.vertices, trianglesData.holes, trianglesData.dimensions);
+
+            triangles[indexData] = [];
+
+            for (var u = 0; u < trianglesResult[indexData].length; u++) {
+                var index = trianglesResult[indexData][u];
+                triangles[indexData].push([trianglesData.vertices[index * trianglesData.dimensions], trianglesData.vertices[index * trianglesData.dimensions + 1]]);
+            }
+
+            /**
+             * ФОРМИРУЕМ МАССИВ ИЗ КООРДИНАТ ЦЕНТРОВ КАЖДОГО ТРЕУГОЛЬНИКА
+             */
+
+            for(var a = 0, b = 1, c = 2, iter = 0; a < triangles[indexData].length; a = a + 3, b = b + 3, c = c + 3, iter++) {
+                arPointsCenters[indexData][iter] = [];
+
+                arPointsCenters[indexData][iter].push(
+                    { x: getMediumX(
+                        triangles[indexData][a][0],
+                        triangles[indexData][b][0],
+                        triangles[indexData][c][0]
+                )});
+
+                arPointsCenters[indexData][iter].push(
+                    { y: getMediumY(
+                        triangles[indexData][a][1],
+                        triangles[indexData][b][1],
+                        triangles[indexData][c][1]
+                )});
+
+                arPointsCenters[indexData][iter].push(
+                    { square: getSquare(
+                            triangles[indexData][a][0], triangles[indexData][a][1],
+                            triangles[indexData][b][0], triangles[indexData][b][1],
+                            triangles[indexData][c][0], triangles[indexData][c][1]
+                )});
+            }
+
+            var item_i = arPointsCenters[indexData];
+
+            item_i.sort(function (a, b) {
+                return b[2].square - a[2].square;
+            });
+
+            resultCenters[indexData] = [];
+            /**
+             * @target_percent - необходимый процент
+             * получить процент количества треугольников от числа всех треугольников
+             */
+            var target_percent = 9;
+            var countPercentCenters = Math.round((target_percent * arPointsCenters[indexData].length) / 100);
+
+            for(var m = 0; m < countPercentCenters; m++){
+                resultCenters[indexData][m] = [];
+                resultCenters[indexData][m].push(
+                    {
+                        x : arPointsCenters[indexData][m][0],
+                        y : arPointsCenters[indexData][m][1],
+                        square : arPointsCenters[indexData][m][2]
+                    });
+            }
+
+
+            indexData++;
         }
     }
 
-    console.log(arAbsolutePoints);
+    /***
+     * СОРТИРОВКА ПО УБЫВАНИЮ
+     */
 
-    // res.send(item);
+    for (var i = 0; i < arPointsCenters.length - 1; i++) {
+        var item_i = arPointsCenters[i];
+
+        item_i.sort(function (a, b) {
+            return b[2].square - a[2].square;
+        });
+    }
+    //
+    var resultCenters = new Array((items.length / 3) - 1);
+
+    for(var r = 0; r < arPointsCenters.length - 1; r++)
+    {
+        resultCenters[r] = [];
+        /**
+         * @target_percent - необходимый процент
+         * получить процент количества треугольников от числа всех треугольников
+         */
+        var target_percent = 9;
+        var countPercentCenters = Math.round((target_percent * arPointsCenters[r].length) / 100);
+
+        if(countPercentCenters < 0) {
+            countPercentCenters = 1;
+        }
+
+        for(var m = 0; m < 1; m++){
+
+            try {
+                resultCenters[r][m] = [];
+                resultCenters[r][m].push(
+                    {
+                        x: arPointsCenters[r][m][0],
+                        y: arPointsCenters[r][m][1],
+                        square: arPointsCenters[r][m][2]
+                    });
+            }
+            catch(e){}
+
+        }
+
+    }
+
+    // иногда появляются в конце null и на сортировке программа крашится, надо разобраться
+    res.send(JSON.stringify(resultCenters));
 });
 
 module.exports = router;
+
+function getMediumX(x1, x2, x3) {
+    return parseInt((x1 + x2 + x3) / 3);
+}
+
+function getMediumY(y1, y2, y3) {
+    return  parseInt((y1 + y2 + y3) / 3);
+}
+
+function getSquare(x1, y1, x2, y2, x3, y3) {
+    var points = [(x1 - x3), (x2 - x3), (y1 - y3), (y2 - y3)];
+    return parseInt( ((points[0] * points[3]) - (points[1] * points[2])) / 2);
+}
